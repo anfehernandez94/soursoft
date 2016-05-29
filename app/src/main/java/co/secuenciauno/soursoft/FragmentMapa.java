ab02446c60d438e9ae7930186e0ebba2b613df43
Andrés Hernández <anfehernandez94@hotmail.com>
2016-05-29 11:17:58 -0500

    Modificación: Se ponen pestañas con la opción de categoria y mapa, y ya se posee la clave para los maps

--- app/src/main/java/co/secuenciauno/soursoft/FragmentMapa.java
+++ app/src/main/java/co/secuenciauno/soursoft/FragmentMapa.java
@@ -23,7 +23,7 @@ public class FragmentMapa extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.fragment_mapa, container, false);
     }
 
 
--- app/src/main/java/co/secuenciauno/soursoft/MainActivity.java
+++ app/src/main/java/co/secuenciauno/soursoft/MainActivity.java
@@ -1,7 +1,9 @@
 package co.secuenciauno.soursoft;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,10 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-
     FragmentManager fragmentManager;
-    FragmentCategoria fragmentStep0;
-    FragmentMapa fragmentStep1;
 
 
     @Override
@@ -20,19 +19,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final FragmentCategoria fragmentStep0;
+        final FragmentMapa fragmentStep1;
+
         fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         fragmentStep0 = new FragmentCategoria();
         fragmentStep1 = new FragmentMapa();
 
-        fragmentTransaction.add(R.id.rl_fragment_problema, fragmentStep0);
-        fragmentTransaction.add(R.id.rl_fragment_problema, fragmentStep1);
+        fragmentTransaction.add(R.id.rl_app_bar_main, fragmentStep0);
+        fragmentTransaction.add(R.id.rl_app_bar_main, fragmentStep1);
 
         fragmentTransaction.show(fragmentStep0);
         fragmentTransaction.hide(fragmentStep1);
 
         fragmentTransaction.commit();
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.appbartabs);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.addTab(tabLayout.newTab().setText("Categorias"));
+        tabLayout.addTab(tabLayout.newTab().setText("Mapa"));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == 0) {
+                    goToFragment(fragmentStep1, fragmentStep0);
+                }else{
+                    goToFragment(fragmentStep0, fragmentStep1);
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
 
     @Override
@@ -51,5 +82,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void goToFragment(Fragment hide, Fragment show) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.show(show);
+        fragmentTransaction.hide(hide);
+        fragmentTransaction.commit();
     }
 }
